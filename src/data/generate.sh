13008fbf17a7a961f605568b6ed25308fdc27e22
Totto16 <tobiausgais@gmail.com>
2025-05-27 16:14:43 +0200

    also generate compilation database schema

--- src/data/generate.sh
+++ src/data/generate.sh
@@ -4,4 +4,6 @@ set -ex
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-uv run datamodel-codegen --input "$SCRIPT_DIR/c_cpp_properties.schema.json" --input-file-type jsonschema --output "$SCRIPT_DIR/schema.py" --use-annotated --output-model-type=pydantic_v2.BaseModel
+uv run datamodel-codegen --input "$SCRIPT_DIR/c_cpp_properties.schema.json" --input-file-type jsonschema --output "$SCRIPT_DIR/c_cpp_properties.schema.py" --use-annotated --output-model-type=pydantic_v2.BaseModel
+
+uv run datamodel-codegen --input "$SCRIPT_DIR/compilation_database.schema.json" --input-file-type jsonschema --output "$SCRIPT_DIR/compilation_database.schema.py" --use-annotated --output-model-type=pydantic_v2.BaseModel
