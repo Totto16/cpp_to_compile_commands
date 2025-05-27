d9820b6ae26dc3d526953c9671941128632cd33d
Totto16 <tobiausgais@gmail.com>
2025-05-27 16:15:02 +0200

    refactor index py into subfiles

--- src/index.py
+++ src/index.py
@@ -1,61 +1,45 @@
 #!/usr/bin/env python3
 
+from logging import Logger
+from pathlib import Path
+from arguments import parse_arguments
+from compilation_database import create_compilation_database
+from logger import setup_custom_logger
+from properties_parser import parse_c_cpp_properties
 import json
-import os
+import sys
 
 
-def read_file(file: str) -> list[str]:
-    with open(file) as f:
-        return f.readlines()
+def main() -> int:
+    args = parse_arguments()
 
+    logger: Logger = setup_custom_logger(args.level)
 
-def resolve_arg(arg: str) -> list[str]:
-    if arg.startswith("@"):
-        return read_file(arg.removeprefix("@"))
-    else:
-        return [arg]
+    model, err = parse_c_cpp_properties(Path(args.file))
 
+    if model is None or err is not None:
+        logger.error(f"An error occured while parsing the input file: {err}"),
+        return 1
 
-def main() -> None:
-    with open(".vscode/c_cpp_properties.json") as f:
-        data = json.load(f)
+    compilation_database, err2 = create_compilation_database(args, model)
 
-    config = data["configurations"][0]
-    include_paths = config["includePath"]
-    defines = config.get("defines", [])
-    compiler_path = config.get("compilerPath", "g++")
-    cppStandard = config.get("cppStandard", None)
-    compilerArgs = config.get("compilerArgs", [])
+    if compilation_database is None or err2 is not None:
+        logger.error(
+            f"An error occured while crreating the complation database: {err2}",
+        )
+        return 1
 
-    additional_args: list[str] = []
+    final_output_file = Path(args.output) / "compile_commands.json"
 
-    for arg in compilerArgs:
-        additional_args.extend(resolve_arg(arg))
+    if not final_output_file.parent.exists():
+        final_output_file.parent.mkdir(exist_ok=True, parents=True)
 
-    if cppStandard is not None:
-        additional_args.append(f"--std={cppStandard}")
+    with Path.open(final_output_file, "w") as f:
+        json.dump(compilation_database.raw, f, indent=2)
 
-    commands: list[dict[str, str]] = []
-
-    for root, _dirs, files in os.walk("src"):
-        for file in files:
-            if file.endswith(".cpp"):
-                filepath = os.path.join(root, file)
-                command = {
-                    "directory": os.getcwd(),
-                    "command": f"{compiler_path} -c {filepath} "
-                    + " ".join(additional_args)
-                    + " "
-                    + " ".join(f"-I{inc}" for inc in include_paths)
-                    + " "
-                    + " ".join(f"-D{define}" for define in defines),
-                    "file": filepath,
-                }
-                commands.append(command)
-
-    with open("compile_commands.json", "w") as f:
-        json.dump(commands, f, indent=2)
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    result = main()
+    sys.exit(result)
