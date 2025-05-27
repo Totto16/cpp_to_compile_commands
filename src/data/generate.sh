0c3b26444f1923d2304af497b2768e60fbe06133
Totto16 <tobiausgais@gmail.com>
2025-05-27 15:41:46 +0200

    fix generated schema

--- src/data/generate.sh
+++ src/data/generate.sh
@@ -4,4 +4,4 @@ set -ex
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-uv run datamodel-codegen --input "$SCRIPT_DIR/c_cpp_properties.schema.json" --input-file-type jsonschema --output "$SCRIPT_DIR/schema.py"
+uv run datamodel-codegen --input "$SCRIPT_DIR/c_cpp_properties.schema.json" --input-file-type jsonschema --output "$SCRIPT_DIR/schema.py" --use-annotated --output-model-type=pydantic_v2.BaseModel
